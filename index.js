aae67be61d06e0dcf7611b0ea9bb2387f686b281
sevgiali <sevgiialii97@gmail.com>
2023-03-15 13:19:53 +0200

    Using 3rd party modules

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
+const slugify = require("slugify");
 /////////////////////////////
-//files
-
-//Blocking synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written!");
-
-//Non-blocking , asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("Your file has been written.");
-//       });
-//     });
-//   });
-// });
-///////////////////////
 //SERVER
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -56,6 +22,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
